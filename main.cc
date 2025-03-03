b889bff6bc9a7ea1bdd8b206b92a285d14b540f8
Jai-JAP <jai.jap.318@gmail.com>
2025-03-03 11:06:01 +0530

    Add sort order preference

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include <libaudcore/i18n.h>
 #include <libaudcore/playlist.h>
 #include <libaudcore/plugin.h>
+#include <libaudcore/preferences.h>
+#include <libaudcore/runtime.h>
 
 #include <QUrl>
 #include <QDateTime>
@@ -9,26 +11,47 @@
 #define EXPORT __attribute__((visibility("default")))
 #define APPLICATION_ID "484736379171897345"
 
+static const char * const defaults[] = {
+    "sort-order", "1",
+    nullptr
+};
+    
+static const ComboItem sort_order[] = {
+    ComboItem(N_("Oldest First"), 1), 
+    ComboItem(N_("Latest First"), -1)
+};
+
+void on_change();
+
+static const PreferencesWidget pref_widgets[] = {
+    WidgetLabel (N_("<b>Sort</b>")),
+    WidgetCombo (N_("Sort Order:"),
+    WidgetInt ("sort", "sort-order", on_change),
+    {{sort_order}})
+};
+
+static const PluginPreferences prefs = {{pref_widgets}};
+
 class SortPlugin : public GeneralPlugin {
-
-public:
+    public:
     static const char about[];
-
+    
     static constexpr PluginInfo info = {
         N_("Sort"),
         "audacious-plugin-sort",
-        about
+        about,
+        &prefs
     };
-
+    
     constexpr SortPlugin() : GeneralPlugin (info, false) {}
-
+    
     bool init();
     void cleanup();
 };
 
 EXPORT SortPlugin aud_plugin_instance;
 
-const char SortPlugin::about[] = N_("Simple Sort Plugin");
+const char SortPlugin::about[] = N_("Simple Plugin to Sort by Modification Time");
 
 time_t get_modified_time(const char* filepath)
 {
@@ -40,18 +63,18 @@ time_t get_modified_time(const char* filepath)
 
 static int compare_by_modified_date(const char * a, const char * b)
 {
-    if(get_modified_time(a) < get_modified_time(b))
-    {
-        return -1;
-    }
-    else
-    {
-        return 1;
+    int sort_order = aud_get_int("sort", "sort-order");
+    
+    if(get_modified_time(a) < get_modified_time(b)) {
+        return -1 * sort_order;
+    } else {
+        return 1 * sort_order;
     }
 }
 
 bool SortPlugin::init() {
-
+    aud_config_set_defaults("sort", defaults);
+    
     Playlist::active_playlist().sort_by_filename(compare_by_modified_date);
     Playlist::active_playlist().activate();
     return true;
@@ -60,3 +83,6 @@ bool SortPlugin::init() {
 void SortPlugin::cleanup() {
 }
 
+void on_change() {
+    Playlist::active_playlist().sort_by_filename(compare_by_modified_date);
+}
